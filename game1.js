3cebaed5ec144ae84260575a2d0bfc2691f0cd95
maman-carlos <91982461+maman-carlos@users.noreply.github.com>
2026-04-13 01:07:25 +0700

    Update game1.js

--- game1.js
+++ game1.js
@@ -71,9 +71,12 @@
         const OBFUSCATING_BASE_64_PREFIX = "UxFdVMwNFNwN0wzODEybV",
         encode = e => OBFUSCATING_BASE_64_PREFIX + btoa(unescape(encodeURIComponent(JSON.stringify(e))));
         function decode$1(e) {
-			console.log(Base64.encode(OBFUSCATING_BASE_64_PREFIX))
-			return JSON.parse(decodeURIComponent(escape(Base64.decode(e.replace(OBFUSCATING_BASE_64_PREFIX, "")))));
-            //return JSON.parse(decodeURIComponent(escape(atob(e.replace(OBFUSCATING_BASE_64_PREFIX, "")))))
+			//console.log(Base64.encode(OBFUSCATING_BASE_64_PREFIX))
+			//return JSON.parse(decodeURIComponent(escape(Base64.decode(e.replace(OBFUSCATING_BASE_64_PREFIX, "")))));
+			const base64String = OBFUSCATING_BASE_64_PREFIX.split(',')[1] || OBFUSCATING_BASE_64_PREFIX;
+			const cleanString = base64String.replace(/\s/g, ''); 
+	
+            return JSON.parse(decodeURIComponent(escape(atob(e.replace(cleanString, "")))))
         }
         var version = "0.4.2";
         const APP_ENV = "production",
